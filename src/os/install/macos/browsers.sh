e7689976f03fa396ac07c7207a615751b60bd486
WayneHarris <wayneharris@live.co.uk>
2021-02-13 14:45:20 +0000

    Update browsers.sh

--- src/os/install/macos/browsers.sh
+++ src/os/install/macos/browsers.sh
@@ -8,28 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Browsers\n"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
 brew_install "Chrome" "google-chrome" "--cask"
-brew_install "Chrome Canary" "google-chrome-canary" "--cask"
-brew_install "Chromium" "chromium" "--cask"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
 brew_install "Firefox" "firefox" "--cask"
-brew_install "Firefox Developer" "firefox-developer-edition" "--cask"
-brew_install "Firefox Nightly" "firefox-nightly" "--cask"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-brew_install "Safari Technology Preview" "safari-technology-preview" "--cask"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
 brew_install "Microsoft Edge" "microsoft-edge" "--cask"
