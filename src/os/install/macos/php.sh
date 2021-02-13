d507ed15271851b9b32f21e66e03d537340f489e
WayneHarris <wayneharris@live.co.uk>
2021-02-13 14:45:40 +0000

    Update php.sh

--- src/os/install/macos/php.sh
+++ src/os/install/macos/php.sh
@@ -13,6 +13,9 @@ brew_install "PHP 7.2" "php@7.2"
 brew_install "PHP 7.3" "php@7.3"
 brew_install "PHP 7.4" "php@7.4"
 
+brew unlink php
+brew link php@7.4
+
 pecl install imagick
 
 # Install Redis mkdir fix first
