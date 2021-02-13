fab1a2476b612bc32f7b0374af1be0b7353f513e
WayneHarris <wayneharris@live.co.uk>
2021-02-13 14:45:44 +0000

    Update finder.sh

--- src/os/preferences/macos/finder.sh
+++ src/os/preferences/macos/finder.sh
@@ -34,17 +34,17 @@ execute "defaults write com.apple.finder NewWindowTarget -string 'PfDe' && \
          defaults write com.apple.finder NewWindowTargetPath -string 'file://$HOME/Desktop/'" \
     "Set 'Desktop' as the default location for new Finder windows"
 
-execute "defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true && \
-         defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true && \
-         defaults write com.apple.finder ShowMountedServersOnDesktop -bool true && \
-         defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true" \
-    "Show icons for hard drives, servers, and removable media on the desktop"
+execute "defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false && \
+         defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false && \
+         defaults write com.apple.finder ShowMountedServersOnDesktop -bool false && \
+         defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false" \
+    "Hide icons for hard drives, servers, and removable media on the desktop"
 
 execute "defaults write com.apple.finder ShowRecentTags -bool false" \
     "Do not show recent tags"
 
-execute "defaults write -g AppleShowAllExtensions -bool true" \
-    "Show all filename extensions"
+execute "defaults write -g AppleShowAllExtensions -bool false" \
+    "Hide all filename extensions"
 
 execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist && \
          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist" \
