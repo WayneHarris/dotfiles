2d19e7c01c376ad0c5ffd8ee36bc31af7bf9c248
WayneHarris <wayneharris@live.co.uk>
2023-09-11 10:05:58 +0100

    use sites

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -1,4 +1,4 @@
 #!/bin/sh
-alias src='cd ~/Code'
-alias blog='cd ~/Code/wayneharris.github.io'
+alias src='cd ~/Sites'
+alias blog='cd ~/Sites/wayneharris.github.io'
 alias terminal-test="for i in $(seq 1 10); do /usr/bin/time zsh -i -c exit; done"
